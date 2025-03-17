6426e175b7e413685b2b42b0b2f6c38cc259f3cf
Abhinav-7187 <abhinav.singh1469@gmail.com>
2025-03-18 01:47:18 +0530

    AWS RDS

---
+++ config/database.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: 3306,
+    logging: false,
+    dialectOptions: {
+      ssl: process.env.NODE_ENV === 'production' ? {
+        require: true,
+        rejectUnauthorized: false
+      } : false
+    }
+  }
+);
+
+module.exports = sequelize;
